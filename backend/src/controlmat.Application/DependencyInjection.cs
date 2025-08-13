6fc765856f04e6b647c0c382af69d5fe9eb4fc65
Luis Le <110530848+Leci37@users.noreply.github.com>
2025-08-13 08:32:16 +0200

    Register AutoMapper and HttpContextAccessor

--- backend/src/controlmat.Application/DependencyInjection.cs
+++ backend/src/controlmat.Application/DependencyInjection.cs
@@ -7,7 +7,7 @@ namespace Controlmat.Application;
 
 public static class DependencyInjection
 {
-    public static IServiceCollection AddApplication(this IServiceCollection services)
+    public static IServiceCollection AddApplicationServices(this IServiceCollection services)
     {
         services.AddMediatR(cfg => cfg.RegisterServicesFromAssembly(Assembly.GetExecutingAssembly()));
         services.AddValidatorsFromAssembly(Assembly.GetExecutingAssembly());
