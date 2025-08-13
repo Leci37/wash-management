58fc17700f8a9a137ea9cd62db6e0085292d942d
Luis Le <110530848+Leci37@users.noreply.github.com>
2025-08-13 09:13:00 +0200

    fix: enforce valid machine and prot dto

--- backend/src/controlmat.Application/Common/Dto/ProtDto.cs
+++ backend/src/controlmat.Application/Common/Dto/ProtDto.cs
@@ -1,10 +1,15 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace Controlmat.Application.Common.Dto;
 
 public class ProtDto
 {
-
-    public int Id { get; set; }
+    [Required]
     public string ProtId { get; set; } = string.Empty;
+
+    [Required]
     public string BatchNumber { get; set; } = string.Empty;
+
+    [Required]
     public string BagNumber { get; set; } = string.Empty;
 }
