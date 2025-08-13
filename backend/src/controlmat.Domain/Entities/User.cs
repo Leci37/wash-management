87c49a7ce0c03d6875973bc407614148005cbf95
Luis Le <110530848+Leci37@users.noreply.github.com>
2025-08-13 09:12:41 +0200

    fix: remove role references from user

--- backend/src/controlmat.Domain/Entities/User.cs
+++ backend/src/controlmat.Domain/Entities/User.cs
@@ -6,7 +6,6 @@ public class User
 {
     public int UserId { get; set; }
     public string UserName { get; set; } = string.Empty;
-    public string? Role { get; set; }
 
     public ICollection<Washing> StartedWashes { get; set; } = new List<Washing>();
     public ICollection<Washing> FinishedWashes { get; set; } = new List<Washing>();
